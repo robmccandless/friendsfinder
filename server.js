3571d6bc5728f3196b9d29237bc92b61316e0ece
Rob McCandless <robmccandless@rocketmail.com>
2020-04-28 14:30:49 -0600

    added bodyParser

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ var path = require("path");
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParer.json({ type: "application.api+json" }));
+
+
 app.listen(port, () => console.log("Listening on port %s", port));
